94c82b65a17bb16ae51b44056be36ea9803baab9
jinen abdelhak <jinenab123@gmail.com>
2019-10-11 20:33:39 +0100

    pipe

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http')
 const fs = require('fs')
-let myReadStream = fs.createReadStream(__dirname + '/readMe.txt')
-myReadStream.on('data', function(chunk) {
-        console.log('new chunk recived')
-        console.log(chunk)
-    })
+let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8')
+let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
+myReadStream.pipe(myWriteStream)
+    // myReadStream.on('data', function(chunk) {
+    //         console.log('new chunk recived')
+    //         myWriteStream.write(chunk)
+    //     })
     // const hostname = '127.0.0.1';
     // const port = 3008;
 
