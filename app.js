73b93c45fbf6d645d3357809163209897f941d6f
jinen abdelhak <jinenab123@gmail.com>
2019-10-11 21:17:00 +0100

    json

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = require('http')
 const fs = require('fs')
 let server = http.createServer(function(req, res) {
-    console.log(200, { 'Content-Type': 'text/html' })
-    let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
-    myReadStream.pipe(res)
+    console.log('request was made ' + req.url)
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    let myObj = {
+        name: "jinen",
+        age: 22
+    }
+    res.end(JSON.stringify(myObj))
 })
 server.listen(3008, '127.0.0.1')
 console.log('yo yo yo port 3008')
