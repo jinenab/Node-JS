2985fe5f0c5a02b9f178f6b6dbfe1801a8e9a323
jinen abdelhak <jinenab123@gmail.com>
2019-10-11 21:01:48 +0100

    get data from an html file

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const http = require('http')
 const fs = require('fs')
-let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8')
-let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
-myReadStream.pipe(myWriteStream)
-    // myReadStream.on('data', function(chunk) {
-    //         console.log('new chunk recived')
-    //         myWriteStream.write(chunk)
-    //     })
-    // const hostname = '127.0.0.1';
-    // const port = 3008;
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
-// server.close()
+let server = http.createServer(function(req, res) {
+    console.log(200, { 'Content-Type': 'text/html' })
+    let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+    myReadStream.pipe(res)
+})
+server.listen(3008, '127.0.0.1')
+console.log('yo yo yo port 3008')
