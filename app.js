9ae78850d4527c2b9237ed476b0c9422e87e9382
jinen abdelhak <jinenab123@gmail.com>
2019-10-12 20:44:39 +0100

    Query Strings

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.get('/', function(req, res) {
     res.render('index')
 })
 app.get('/contact', function(req, res) {
-    res.render('contact')
+
+    res.render('contact', { qs: req.query })
+
 })
 app.get('/profile/:id', function(req, res) {
     var data = { age: 22, job: "developper", hobbies: ['sport', 'music'] }
