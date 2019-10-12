380b390a9eaf767cd513f59a42c30f89dad65a51
jinen abdelhak <jinenab123@gmail.com>
2019-10-12 09:45:34 +0100

    get familiar with routes and node's routing

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const http = require('http')
-const fs = require('fs')
+let http = require('http')
+let fs = require('fs')
 let server = http.createServer(function(req, res) {
     console.log('request was made ' + req.url)
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    let myObj = {
-        name: "jinen",
-        age: 22
+
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        fs.createReadStream(__dirname + '/index.html').pipe(res)
+
+    } else if (req.url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        fs.createReadStream(__dirname + '/contact.html').pipe(res)
+    } else if (req.url === '/api/ninjas') {
+        let ninjas = [{ name: 'ryu', age: '29' }, { name: 'jinen', age: 22 }]
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        JSON.stringify(ninjas)
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        fs.createReadStream(__dirname + '/404.html').pipe(res)
+
     }
-    res.end(JSON.stringify(myObj))
 })
+
 server.listen(3008, '127.0.0.1')
-console.log('yo yo yo port 3008')
